147e7b48fd82b17ac6bd5a34cd783290d016e3fe
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-03 14:53:34 -0300

    refactor: insert interval in content

--- content.js
+++ content.js
@@ -1,21 +1,27 @@
-const header = document.querySelector("#start");
+const interval = setInterval(() => {
+  const header = document.querySelector("#start");
 
-const button = document.createElement("button");
-button.innerHTML = "2x";
-button.classList.add("twoTimesButton");
-
-button.addEventListener("click", () => {
-  const videos = document.querySelectorAll("video");
-  videos.forEach(video => {
-    const inactive = video.playbackRate === 2;
-    if(inactive) {
-      video.playbackRate = 1;
-      button.classList.remove("speedChangerActive");
-    } else {
-      video.playbackRate = 2;
-      button.classList.add("speedChangerActive");
-    }
-  })
-})
-
-header?.appendChild(button);
+  if(header) {
+    clearInterval(interval);
+    
+    const button = document.createElement("button");
+    button.innerHTML = "2x";
+    button.classList.add("twoTimesButton");
+  
+    button.addEventListener("click", () => {
+      const videos = document.querySelectorAll("video");
+      videos.forEach(video => {
+        const inactive = video.playbackRate === 2;
+        if(inactive) {
+          video.playbackRate = 1;
+          button.classList.remove("speedChangerActive");
+        } else {
+          video.playbackRate = 2;
+          button.classList.add("speedChangerActive");
+        }
+      })
+    })
+  
+    header.appendChild(button);
+  }
+}, 1000);
