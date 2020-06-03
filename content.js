8d495e91e3f1041dd4a02ba97109a60e0a725c6d
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-06-03 12:00:42 -0300

    create extension

---
+++ content.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("#start");
+
+const button = document.createElement("button");
+button.innerHTML = "2x";
+button.classList.add("twoTimesButton");
+
+button.addEventListener("click", () => {
+  const videos = document.querySelectorAll("video");
+  videos.forEach(video => {
+    const inactive = video.playbackRate === 2;
+    if(inactive) {
+      video.playbackRate = 1;
+      button.classList.remove("speedChangerActive");
+    } else {
+      video.playbackRate = 2;
+      button.classList.add("speedChangerActive");
+    }
+  })
+})
+
+header?.appendChild(button);
